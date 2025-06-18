e527794717c6010b91ca32875f8725d3732edb22
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-06-18 20:19:25 +0545

    Connected Database

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
-import express from 'express';
-import 'dotenv/config';
+const express = require('express');
+const dotenv = require('dotenv');
+const { chats } = require('./data/data');
+const dbConnect = require('./config/db');
 
 const app = express();
+dbConnect();
+
+dotenv.config();
 const port = process.env.PORT;
 
-app.get('/about', (req, res) => {
-  res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('This is Contact Us Page!');
+});
+
+app.get('/chats', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/chats/:id', (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
 });
 
 app.listen(port, () => {
