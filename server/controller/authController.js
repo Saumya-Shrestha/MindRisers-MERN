5377af20e339353d9da8e4007d9440bd69769693
Saumya Man Shrestha <saumyamanshrestha@gmail.com>
2025-06-20 19:53:15 +0545

    Added login with auth-token

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,18 +1,70 @@
-const User = require('../model/user');
+const User = require("../model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const secret = process.env.secret;
 
 const createUser = async (req, res) => {
-  const { name, email, password } = req.body;
+  // const { name, email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ email: req.body.email });
     if (user) {
-      return res.status(400).json({ message: 'User Already Exists' });
+      return res.status(400).json({ message: "User Already Exists" });
     }
-    user = await User.create({ name, email, password });
-    res.status(201).json({ message: 'User Created Succesfully', user });
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    const data = {
+      user: {
+        id: user._id,
+      },
+    };
+    var authToken = jwt.sign(data, secret);
+    res.status(201).json({ message: "User Created Succesfully", user, authToken });
   } catch (error) {
-    console.log('Error: ', error);
-    res.status(500).json({ message: 'Server Error' });
+    console.log("Error: ", error);
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
-module.exports = { createUser };
+const login = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    const passwordCompare = await bcrypt.compare(req.body.password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+    const data = {
+      user: {
+        id: user._id,
+      },
+    };
+    var authToken = jwt.sign(data, secret);
+    res.status(201).json({ message: "Login Successful", user, authToken });
+  } catch (error) {
+    console.log("Error: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = { createUser, login };
--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,8 +1,19 @@
-const express = require('express');
-const { createUser } = require('../controller/authController');
+const express = require("express");
+const { createUser, login } = require("../controller/authController");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-router.post('/createuser', createUser);
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 3 }).withMessage("Name must be atleast 3 characters"),
+    body("password").isLength({ min: 3 }).withMessage("Password must be atleast 3 characters long."),
+    body("email").isEmail().withMessage("Please enter a valid email"),
+  ],
+  createUser
+);
+
+router.post("/login", [body("email").isEmail().withMessage("Please enter a valid email")], login);
 
 module.exports = router;
